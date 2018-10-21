82f527f42fd5b8fe224aae43ef846763c12cd469
elfdmrtas <merve.elif.demirtas@gmail.com>
2018-10-21 13:25:07 +0300

    last

--- main.py
+++ main.py
@@ -11,15 +11,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
-##########################################
-
-########################################
 
 class App(QMainWindow):
     def __init__(self):
         super(App, self).__init__()
 
-        self.title = 'Histogram Equalization'
+        self.title = 'Histogram Matching'
         self.left = 10
         self.top = 10
         self.width = 1000
@@ -30,43 +27,74 @@ class App(QMainWindow):
 
         self.initUI()
 
-
     def openInputImage(self):
         # This function is called when the user clicks File->Input Image.
 
         self.inputLoaded = True
 
+        # ******** place image into qlabel object *********************
         imagePath, _ = QFileDialog.getOpenFileName()
-        inputImg = cv2.imread(imagePath)
+        self.inputImg = cv2.imread(imagePath)
 
         pixmap_label = self.qlabel1
 
-        height, width, channel = inputImg.shape
+        height, width, channel = self.inputImg.shape
         bytesPerLine = 3 * width
-        qImg = QImage(inputImg.data, width, height, bytesPerLine, QImage.Format_RGB888).rgbSwapped()
+        qImg = QImage(self.inputImg.data, width, height, bytesPerLine, QImage.Format_RGB888).rgbSwapped()
         pixmap = QPixmap(qImg)
 
         pixmap = pixmap.scaled(300, 300, Qt.KeepAspectRatio)
         pixmap_label.setPixmap(pixmap)
+        # **************************************************************
 
+        # **************** create histogram ***************************
+        # allocate for histogram
+        self.hist1 = np.zeros(([256, channel]))
+
+        # create the histogram
+        for g in range(256):
+            for b in range(3):  # through channels
+                self.hist1[g, b] = np.sum(np.sum(self.inputImg[:, :, b] == g, 0), 0)
+        # **************************************************************
+
+        # ************* add histogram to hbox object ******************
+        self.myfig = PlotCanvas(self, width=5, height=4, dpi=100, histr=self.hist1, title="Input Image")
+        self.hBoxlayout2.addWidget(self.myfig)
 
     def openTargetImage(self):
         # This function is called when the user clicks File->Target Image.
 
         self.targetLoaded = True
 
+        # ******** place image into qlabel object *********************
         imagePath, _ = QFileDialog.getOpenFileName()
-        inputImg = cv2.imread(imagePath)
+        self.targetImg = cv2.imread(imagePath)
 
         pixmap_label = self.qlabel2
-        height, width, channel = inputImg.shape
+
+        height, width, channel = self.targetImg.shape
         bytesPerLine = 3 * width
-        qImg = QImage(inputImg.data, width, height, bytesPerLine, QImage.Format_RGB888).rgbSwapped()
+        qImg = QImage(self.targetImg.data, width, height, bytesPerLine, QImage.Format_RGB888).rgbSwapped()
         pixmap = QPixmap(qImg)
 
         pixmap = pixmap.scaled(300, 300, Qt.KeepAspectRatio)
         pixmap_label.setPixmap(pixmap)
+        #**************************************************************
 
+        # **************** create histogram ***************************
+        # allocate for histogram
+        self.hist2 = np.zeros(([256, channel]))
+
+        # create the histogram
+        for g in range(256):
+            for b in range(3):  # through channels
+                self.hist2[g, b] = np.sum(np.sum(self.targetImg[:, :, b] == g, 0), 0)
+
+        #**************************************************************
+
+        # ************* add histogram to hbox object ******************
+        self.myfig = PlotCanvas(self, width=5, height=4, dpi=100, histr=self.hist2, title="Target Image")
+        self.hBoxlayout2.addWidget(self.myfig)
 
     def initUI(self):
         # Write GUI initialization code
@@ -78,29 +106,38 @@ class App(QMainWindow):
         wid = QWidget(self)
         self.setCentralWidget(wid)
 
-        b1 = QPushButton("Equalize Histogram")
+        b1 = QPushButton("Match Histogram")
         b1.clicked.connect(self.histogramButtonClicked)
 
         self.groupBox = QGroupBox()
-        hBoxlayout = QHBoxLayout()
+        self.hBoxlayout = QHBoxLayout()
 
         self.qlabel1 = QLabel('Input', self)
         self.qlabel1.setStyleSheet("border: 1px inset grey; min-height: 200px; ")
-        hBoxlayout.addWidget(self.qlabel1)
+        self.qlabel1.setAlignment(Qt.AlignCenter)
+        self.hBoxlayout.addWidget(self.qlabel1)
 
         self.qlabel2 = QLabel('Target', self)
         self.qlabel2.setStyleSheet("border: 1px inset grey; min-height: 200px; ")
-        hBoxlayout.addWidget(self.qlabel2)
+        self.qlabel2.setAlignment(Qt.AlignCenter)
+        self.hBoxlayout.addWidget(self.qlabel2)
 
         self.qlabel3 = QLabel('Result', self)
         self.qlabel3.setStyleSheet("border: 1px inset grey; min-height: 200px; ")
-        hBoxlayout.addWidget(self.qlabel3)
+        self.qlabel3.setAlignment(Qt.AlignCenter)
+        self.hBoxlayout.addWidget(self.qlabel3)
 
-        self.groupBox.setLayout(hBoxlayout)
+        self.groupBox.setLayout(self.hBoxlayout)
+
+        #************add histogram layers*************
+        self.groupBox2 = QGroupBox()
+        self.hBoxlayout2 = QHBoxLayout()
+        self.groupBox2.setLayout(self.hBoxlayout2)
 
         vBox = QVBoxLayout()
         vBox.addWidget(b1)
         vBox.addWidget(self.groupBox)
+        vBox.addWidget(self.groupBox2)
 
         wid.setLayout(vBox)
 
@@ -121,36 +158,138 @@ class App(QMainWindow):
         exitAct.setStatusTip('Exit application')
         exitAct.triggered.connect(qApp.quit)
         fileMenu.addAction(exitAct)
-
         #------------------------------------
-        self.show()
 
+        self.show()
 
     def histogramButtonClicked(self):
         if not self.inputLoaded and not self.targetLoaded:
             # Error: "First load input and target images" in MessageBox
             QMessageBox.question(self, 'Error Message', "Please, load input image and target image", QMessageBox.Ok, QMessageBox.Ok)
-
         elif not self.inputLoaded:
             # Error: "Load input image" in MessageBox
             QMessageBox.question(self, 'Error Message', "Please, load input image", QMessageBox.Ok, QMessageBox.Ok)
         elif not self.targetLoaded:
             # Error: "Load target image" in MessageBox
             QMessageBox.question(self, 'Error Message', "Please, load target image", QMessageBox.Ok, QMessageBox.Ok)
+        else:
+            self.calcHistogram()
 
-    def calcHistogram(self, I):
-        # Calculate histogram
-        return NotImplementedError
+    def calcHistogram(self):
+        # Calculate matching histogram
+
+        input_height, input_width, input_channel = self.inputImg.shape
+        target_height, target_width, target_channel = self.targetImg.shape
+
+        # allocate for probability density function(pdf)
+        pdf1 = np.zeros(([256, input_channel]))
+        pdf2 = np.zeros(([256, target_channel]))
+
+        # create pdf for input image
+        for g in range(256):
+            for b in range(3):  # through channels
+                pdf1[g, b] = ((self.hist1[g, b]) / (input_height * input_width))    # input
+                pdf2[g, b] = ((self.hist2[g, b]) / (target_height * target_width))  # target
+
+        # create cdf
+        cdf1 = np.zeros(([256, input_channel]))
+        cdf2 = np.zeros(([256, target_channel]))
+
+        cdf1[:, 0] = np.cumsum(pdf1[:, 0])
+        cdf1[:, 1] = np.cumsum(pdf1[:, 1])
+        cdf1[:, 2] = np.cumsum(pdf1[:, 2])
+
+        cdf2[:, 0] = np.cumsum(pdf2[:, 0])
+        cdf2[:, 1] = np.cumsum(pdf2[:, 1])
+        cdf2[:, 2] = np.cumsum(pdf2[:, 2])
+
+        # create look up table(LUT)
+        LUT_R = np.zeros((256, 1), dtype=np.uint8)
+        LUT_G = np.zeros((256, 1), dtype=np.uint8)
+        LUT_B = np.zeros((256, 1), dtype=np.uint8)
+
+        for i in range(255):
+            for j in range(255):
+                if(cdf1[i, 0] < cdf2[j, 0]):
+                    LUT_B[i] = j
+                    break;
+
+        for i in range(255):
+            for j in range(255):
+                if(cdf1[i, 1] < cdf2[j, 1]):
+                    LUT_G[i] = j
+                    break;
+
+        for i in range(255):
+            for j in range(255):
+                if(cdf1[i, 2] < cdf2[j, 2]):
+                    LUT_R[i] = j
+                    break;
+
+
+        self.result_image = np.zeros((target_height, target_width, 3), dtype=np.uint8)
+
+        #look look up table and match the histogram
+        for height in range(target_height):
+            for width in range(target_width):
+                pixel1 = self.inputImg[height, width, 0]
+                pixel2 = self.inputImg[height, width, 1]
+                pixel3 = self.inputImg[height, width, 2]
+
+                self.result_image[height, width, 0] = LUT_B[pixel1]
+                self.result_image[height, width, 1] = LUT_G[pixel2]
+                self.result_image[height, width, 2] = LUT_R[pixel3]
+
+
+        #place the result image into box
+        pixmap_label = self.qlabel3
+
+        bytesPerLine = 3 * target_width
+        qImg = QImage(self.result_image.data, target_width, target_height, bytesPerLine, QImage.Format_RGB888).rgbSwapped()
+        pixmap = QPixmap(qImg)
+
+        pixmap = pixmap.scaled(300, 300, Qt.KeepAspectRatio)
+        pixmap_label.setPixmap(pixmap)
+
+        # ******allocate for histogram**********
+        self.hist3 = np.zeros(([256, target_channel]))
+
+        # create the histogram
+        for g in range(256):
+            for b in range(3):  # through channels
+                self.hist3[g, b] = np.sum(np.sum(self.result_image[:, :, b] == g, 0), 0)
+
+        self.myfig = PlotCanvas(self, width=5, height=4, dpi=100, histr=self.hist3, title="Result Image")
+        self.hBoxlayout2.addWidget(self.myfig)
+
 
 class PlotCanvas(FigureCanvas):
-    def __init__(self, hist, parent=None, width=5, height=4, dpi=100):
-        return NotImplementedError
-        # Init Canvas
-        self.plotHistogram(hist)
+    def __init__(self, parent=None, width=5, height=4, dpi=100, histr=0, title=""):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes1 = fig.add_subplot(311)
+        self.axes2 = fig.add_subplot(312)
+        self.axes3 = fig.add_subplot(313)
 
-    def plotHistogram(self, hist):
-        return NotImplementedError
-        # Plot histogram
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(self, QSizePolicy.Expanding, QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+        self.plotHistogram(histr, title)
+
+
+    def plotHistogram(self, histr, title):
+        ######## create histogram ########
+
+        ax1 = self.axes1
+        ax1.bar(range(0, 256), histr[:, 0], color=[0, 0, 1])
+        ax1.set_title(title)
+
+        ax2 = self.axes2
+        ax2.bar(range(0, 256), histr[:, 1], color=[0, 1, 0])
+
+        ax3 = self.axes3
+        ax3.bar(range(0, 256), histr[:, 2], color=[1, 0, 0])
 
         self.draw()
 
